f159fff29de3b7907f18d47b2698a693e4e62ad4
Dennis <ngetichdennis90@gmail.com>
2024-03-20 19:07:28 +0300

    generating the display menu which will be displayed on the CLI

---
+++ Lib/cli.py
@@ -0,0 +1,10 @@
+from helper import add_owner, add_property, list_all_owners, list_all_properties
+
+def display_menu():
+    print("\nWelcome to Property Management System")
+    print("1. Add Owner")
+    print("2. Add Property")
+    print("3. List All Owners")
+    print("4. List All Properties")
+    print("5. Exit")
+
