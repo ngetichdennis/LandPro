70c70ee01663270f6cdfba67848095e324e1218e
Dennis <ngetichdennis90@gmail.com>
2024-03-20 16:01:25 +0300

    updating of methods to be called by the class itself for creating tables using the class attribute and also dropping table

--- Lib/models/owner.py
+++ Lib/models/owner.py
@@ -11,3 +11,26 @@ class Owner:
         self.name = name
         self.contact=contact
         self.identification_no=identification_no
+
+
+    @classmethod
+    def create_table(cls):
+        """ Create a new table to persist the attributes of owner instances """
+        sql = """
+            CREATE TABLE IF NOT EXISTS owners (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            contact INT,
+            identification_no INT)
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        """ Drop the table that persists Owner instances """
+        sql = """
+            DROP TABLE IF EXISTS owners;
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
