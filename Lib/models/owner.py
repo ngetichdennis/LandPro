8c3c1d672773577ddd66e6c372de0d027e92924c
Dennis <ngetichdennis90@gmail.com>
2024-03-20 18:10:12 +0300

    geneating of methods that manipulates the instances of the class,creating new objects and persisting to the database

--- Lib/models/owner.py
+++ Lib/models/owner.py
@@ -34,3 +34,37 @@ class Owner:
         """
         CURSOR.execute(sql)
         CONN.commit()
+    
+    
+    def save(self):
+        """ Insert a new row with the name,contact and identification_no values of the current Owner instance.
+        Update object id attribute using the primary key value of new row.
+        Save the object in local dictionary using table row's PK as dictionary key"""
+        sql = """
+            INSERT INTO owners (name,contact,identification_no)
+            VALUES (?, ?, ?)
+        """
+
+        CURSOR.execute(sql, (self.name,self.contact,self.identification_no))
+        CONN.commit()
+
+        self.id = CURSOR.lastrowid
+        type(self).all[self.id] = self
+
+    @classmethod
+    def create(cls,name,contact,identification_no):
+        """ Initialize a new Owner instance and save the object to the database """
+        owner = cls(name,contact,identification_no)
+        owner.save()
+        return owner
+
+    def update(self):
+        """Update the table row corresponding to the current owner instance."""
+        sql = """
+            UPDATE owners
+            SET name = ?, contact = ?, identification_no = ?
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.name,self.contact,self.identification_no, self.id))
+        CONN.commit()
+
